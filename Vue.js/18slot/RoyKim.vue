8ca473a9997c950eab6f4b9ed4b443b98e8e01a9
KyuHwan Shim <46957634+underthelights@users.noreply.github.com>
2021-02-16 23:22:46 +0900

    Create RoyKim.vue

---
+++ Vue.js/18slot/RoyKim.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <!-- <h1>{{ title }}</h1>
+         -->
+        <p>header</p>
+        <slot name="header" :roy="roy"></slot>
+        <p>header</p>
+        <slot name="header"></slot>
+        <p>footer</p>
+        <!-- <p> {{ name }}</p><br>
+        <button @click="updateName">His Real Name</button> -->
+    </div>
+    
+</template>
+
+<script>
+export default {
+    // props:['title'],
+    props:{
+        title:{
+            type: String,
+            default: 'default title'
+        },
+        name:{
+            type: String,
+            default: 'default name'
+        }
+    },
+    data(){
+        return{
+            //name: 'Roy Kim',
+            roy : 'singer'
+        }
+    },
+    methods:{
+        updateName(){
+            this.name = 'Sung-Woo Kim'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
