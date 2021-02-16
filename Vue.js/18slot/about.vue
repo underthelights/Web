a38cbebd5d4039482c8f788f127485e132afdb93
KyuHwan Shim <46957634+underthelights@users.noreply.github.com>
2021-02-16 23:22:57 +0900

    Create about.vue

---
+++ Vue.js/18slot/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <!-- <RoyKim title="About Title"/> -->
+    <RoyKim>
+      <template v-slot:header="props">
+        <p> header 1111!!! / {{ props.roy }} </p>
+      </template>
+      <template #default>
+        hello mello
+      </template>
+
+    </RoyKim>
+
+  </div>
+</template>
+
+<script>
+import RoyKim from '@/components/RoyKim.vue';
+export default {
+  components:{
+    RoyKim
+  }
+}
+</script>
